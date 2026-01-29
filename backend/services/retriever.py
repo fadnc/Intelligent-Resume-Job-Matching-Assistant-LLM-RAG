19515d5c1628ffce7b6dbd07f30a8a311f9d4eff
Fad <ncfadhil@gmail.com>
2026-01-29 12:43:40 +0530

    Added caching to retriever file for better loading for the cached content

--- backend/services/retriever.py
+++ backend/services/retriever.py
@@ -1,8 +1,18 @@
 import faiss
+import hashlib
 import numpy as np
 from config import FAISS_PATH, META_PATH
 
-def create_index(vectors, chunks):
+_INDEX_CACHE = {}
+
+def hash_text(text):
+    return hashlib.md5(text.encode()).hexdigest()
+
+def create_index(vectors, chunks, resume_text):
+    key = hash_text(resume_text)
+    if key in _INDEX_CACHE:
+        return key
+    
     dim = vectors.shape[1]
     index = faiss.IndexFlatL2(dim)
     index.add(vectors)
@@ -10,9 +20,11 @@ def create_index(vectors, chunks):
     faiss.write_index(index, FAISS_PATH)
     np.save(META_PATH, np.array(chunks, dtype=object))
     
-def search(query_vec, k=5):
-    index = faiss.read_index(FAISS_PATH)
-    chunks = np.load(META_PATH, allow_pickle=True)
+def search(query_vec, key, k=5):
+    index, chunks = _INDEX_CACHE[key]
+    
+    # index = faiss.read_index(FAISS_PATH)
+    # chunks = np.load(META_PATH, allow_pickle=True)
     
     distances, indices = index.search(np.array([query_vec]), k)
     
