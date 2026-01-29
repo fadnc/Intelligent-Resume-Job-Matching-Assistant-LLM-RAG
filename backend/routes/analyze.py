1c042612aaf0f919d9a35e3a71aa8d01d2a0d547
Fad <ncfadhil@gmail.com>
2026-01-29 10:52:55 +0530

    defined async function to analyze the input resume and job description

---
+++ backend/routes/analyze.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, UploadFile, File, Form
+from services.pipeline import analyze_resume
+
+router = APIRouter()
+
+@router.post("/analyze")
+async def analyze(
+    resume : UploadFile = File(...),
+    job_description: str = Form(...)
+):
+    result = await analyze_resume(resume, job_description)
+    return result
