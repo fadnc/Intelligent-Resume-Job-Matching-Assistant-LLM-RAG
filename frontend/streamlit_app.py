bd2a60725e01105e38bd9cc15214014fb3cb80f1
Fad <ncfadhil@gmail.com>
2026-01-29 14:30:12 +0530

    fix(frontend): retrieve error code

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -8,25 +8,18 @@ jd = st.text_area("Paste Job Description")
 
 if st.button("Analyze") and resume and jd:
 
-    with st.spinner("Analyzing resume..."):
-        files = {
-            "resume": ("resume.pdf", resume.getvalue(), "application/pdf")
-        }
+    files = {"resume": resume}   # IMPORTANT: not resume.getvalue()
+    data = {"job_description": jd}
 
-        data = {
-            "job_description": jd
-        }
+    response = requests.post(
+        "http://localhost:8000/analyze",
+        files=files,
+        data=data
+    )
 
-        response = requests.post(
-            "http://localhost:8000/analyze",
-            files=files,
-            data=data
-        )
-        
-        if response.status_code ==200:
-            st.json(response.json())
-        else:
-            st.error(f"Error: {response.status_code} - {response.text}")
-        
+    st.write("Status:", response.status_code)
 
-    st.json(response.json())
+    try:
+        st.json(response.json())
+    except:
+        st.text(response.text)
