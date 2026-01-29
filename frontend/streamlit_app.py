bdbe7ea9cd53fe9d109d1dac2b3c26c3297dd54e
Fad <ncfadhil@gmail.com>
2026-01-29 12:50:06 +0530

    modified streamlit

---
+++ frontend/streamlit_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests
+
+st.title("Resume Job Matcher")
+
+resume = st.file_uploader("Upload Resume (PDF)", type=["pdf"])
+jd = st.text_area("Paste Job Description")
+
+if st.button("Analyze") and resume and jd:
+
+    with st.spinner("Analyzing resume..."):
+        files = {
+            "resume": ("resume.pdf", resume.getvalue(), "application/pdf")
+        }
+
+        data = {
+            "job_description": jd
+        }
+
+        response = requests.post(
+            "http://localhost:8000/analyze",
+            files=files,
+            data=data
+        )
+        
+
+    st.json(response.json())
