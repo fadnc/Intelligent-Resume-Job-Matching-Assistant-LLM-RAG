c1f9d96cd80a954c84dec8c0fb515a30f8d2d49f
Fad <ncfadhil@gmail.com>
2026-01-29 11:39:45 +0530

    add sentence-transformer based embedding utility for chunked text

---
+++ backend/services/embeddings.py
@@ -0,0 +1,12 @@
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from config import EMBED_MODEL
+
+model = SentenceTransformer(EMBED_MODEL)
+
+def embed_texts(texts):
+    vectors = model.encode(texts, 
+                           convert_to_numpy=True,
+                           show_progress_bar=True,
+                           batch_size=32)
+    return np.array(vectors).astype("float32")    # imp as vector db expects float32 , if not float64 is slower and eats memory
